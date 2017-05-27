fc16c54262b881352a87ef91bfab6983ab2f5585
s3208866 <danyule@live.com.au>
2017-05-27 15:24:16 +1000

    New events

--- eventHandler.js
+++ eventHandler.js
@@ -1,35 +1,74 @@
 //functions for keyboard events
 
 document.onkeydown = handleKeyDown;
-//document.onkeyup = handleKeyUp;
-
+var currentJoint;
+var direction;
 function handleKeyDown(event){
+	
 	switch(event.keyCode){
-		//left
+		//TURTLE CONTROLS-------------------------------
+		//Front/Back/Left/Right selection
 		case 37:
-			scene.rotation.y += 2*Math.PI/180;
+			direction = "left";
 			break;
-			
-		//right	
 		case 39:
-			scene.rotation.y -= 2*Math.PI/180;
+			direction = "right";
 			break;
-			
-		//up
 		case 38:
-			scene.rotation.x += 2*Math.PI/180;
+			direction = "front";
 			break;
-			
-		//down
 		case 40:
-			scene.rotation.x -= 2*Math.PI/180;
+			direction = "rear";
 			break;
 			
-		//animate stuff
+		//Joint selection
 		case 78:
-			turtle.getObjectByName("neck",true).rotation.z +=2*Math.PI/90;
+			currentJoint = "neck";
 			break;
 			
+		case 84:
+			currentJoint = "tail";
+			break;
+		
+		case 72:
+			currentJoint = "hip";
+			break;
+			
+		case 75:
+			currentJoint = "knee";
+			break;
+			
+		case 65:
+			currentJoint = "ankle";
+			break;
+			
+		//Leg/head/tail joint movements
+		//Up
+		case 187:
+			
+			if(currentJoint == "neck" || currentJoint == "tail"){
+				upJoint(currentJoint);
+			}
+			
+			else if(currentJoint && direction){
+				legJointUp(currentJoint,direction);
+			}
+			break;
+			
+		//Down	
+		case 189:
+		
+			if(currentJoint == "neck" || currentJoint == "tail"){
+				downJoint(currentJoint);
+			}
+			
+			else if(currentJoint && direction){
+				legJointDown(currentJoint,direction);
+			}
+			break;
+
+			
+		//TOGGLE CASES----------------------------------	
 		//Toggle wireframe
 		case 77:
 			turtle.traverse(function(obj){
@@ -69,6 +108,98 @@ function handleKeyDown(event){
 			break;
 	}
 }
+
+//Move leg joints up
+function legJointUp(joint,direction){
+	var maxRotation;
+	if(joint=="hip"){maxRotation = Math.PI*0.5;}
+	if(joint=="knee"){maxRotation = 0;}
+	if(joint=="ankle"){maxRotation = 1.05;}
+
+	for(var x=0; x<turtle.children.length; x++){
+		if(direction == "right"){
+			if(turtle.children[x].name == "fr" || turtle.children[x].name == "rr"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z <= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z += Math.PI/180;
+				}
+			}
+		}
+		if(direction == "left"){
+			if(turtle.children[x].name == "fl" || turtle.children[x].name == "rl"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z <= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z += Math.PI/180;
+				}
+			}
+		}
+		if(direction == "front"){
+			if(turtle.children[x].name == "fl" || turtle.children[x].name == "fr"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z <= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z += Math.PI/180;
+				}
+			}
+		}
+		if(direction == "rear"){
+			if(turtle.children[x].name == "rl" || turtle.children[x].name == "rr"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z <= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z += Math.PI/180;
+				}
+			}
+		}
+	}
+}
+
+//Move leg joint down
+function legJointDown(joint,direction){
+	var maxRotation;
+	if(joint=="hip"){maxRotation = -Math.PI*0.5;}
+	if(joint=="knee"){maxRotation = -Math.PI*0.5;}
+	if(joint=="ankle"){maxRotation = -1.05;}
+
+	for(var x=0; x<turtle.children.length; x++){
+		if(direction == "right"){
+			if(turtle.children[x].name == "fr" || turtle.children[x].name == "rr"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z >= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z -= Math.PI/180;
+				}
+			}
+		}
+		if(direction == "left"){
+			if(turtle.children[x].name == "fl" || turtle.children[x].name == "rl"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z >= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z -= Math.PI/180;
+				}
+			}
+		}
+		if(direction == "front"){
+			if(turtle.children[x].name == "fr" || turtle.children[x].name == "fl"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z >= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z -= Math.PI/180;
+				}
+			}
+		}
+		if(direction == "rear"){
+			if(turtle.children[x].name == "rr" || turtle.children[x].name == "rl"){
+				if(turtle.children[x].getObjectByName(joint,true).rotation.z >= maxRotation){
+					turtle.children[x].getObjectByName(joint,true).rotation.z -= Math.PI/180;
+				}
+			}
+		}
+	}
+}
+
+//Head & Neck movements
+function upJoint(joint){
+	if(turtle.getObjectByName(joint,true).rotation.z <= Math.PI*0.5){
+		turtle.getObjectByName(joint,true).rotation.z += Math.PI/180;
+	}
+}
+
+function downJoint(joint){
+	if(turtle.getObjectByName(joint,true).rotation.z >= -Math.PI*0.5){
+		turtle.getObjectByName(joint,true).rotation.z -= Math.PI/180;
+	}
+}
+
 
 
 
